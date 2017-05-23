24a37fa566d7b1c7850ca2b84392bd6ffdbac780
Kingsley <chukwumakingsley1@gmail.com>
2017-05-23 19:00:30 +0200

    removed gitinore

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  before_filter :configure_devise_params, if: :devise_controller?
+
+
+  def configure_devise_params
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:first_name, :last_name, :gender, :email, :password, :password_confirmation, :phone)
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,36 @@
+Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
+  devise_for :users, ActiveAdmin::Devise.config.merge({
+    controllers: {sessions: 'users/sessions'}, path: '',
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout'
+    }
+   } )
+  # resources :upcoming_events do
+  #   collection do
+  #     get  'past_events'
+  #     get 'upcomings'
+  #     get 'live_events'
+  #   end
+  # end
+  # resources :pages
+  # resources :news
+  # get 'pages/filter/:scope' => 'pages#index', as: :filtered_page
+  # resources :imagegalleries
+   root to: 'home#index'
+  # resources :posts, :pictures, :aboutpages, shallow: true do
+  #   resources :pictures
+  #   member do
+  #     post 'like'
+  #     delete 'unlike'
+  #   end
+  #   resources :comments do
+  #     member do
+  #       post 'like'
+  #       delete 'unlike'
+  #     end
+  #     resources :replies
+  #   end
+  # end
+end
