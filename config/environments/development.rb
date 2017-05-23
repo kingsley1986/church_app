7eb570bcc40641dfa0db0fea6ab8daa649a623e6
Kingsley <chukwumakingsley1@gmail.com>
2017-05-23 22:40:33 +0200

    added awes crendentiails to enviroments productions and development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,16 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+        :provider               => 'AWS',
+        :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
+        :aws_secret_access_key  => ENV['S3_SECRECT'],
+        :endpoint               => "https://s3.amazonaws.com",
+        :region                 => ENV['S3-REGION']
+    }
+    config.fog_directory  = ENV['S3_BUCKET']
+  end
 end
