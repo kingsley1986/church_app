b38484dc6585d327ec65514ff10e55a0ca0d9933
Kingsley <chukwumakingsley1@gmail.com>
2017-05-24 12:33:13 +0200

    changed background iamge

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,57 +16,106 @@
 //= require turbolinks
 //= require_tree .
 
-jssor_1_slider_init = function() {
 
-    var jssor_1_SlideoTransitions = [
-      [{b:0,d:600,y:-290,e:{y:27}}],
-      [{b:0,d:1000,y:185},{b:1000,d:500,o:-1},{b:1500,d:500,o:1},{b:2000,d:1500,r:360},{b:3500,d:1000,rX:30},{b:4500,d:500,rX:-30},{b:5000,d:1000,rY:30},{b:6000,d:500,rY:-30},{b:6500,d:500,sX:1},{b:7000,d:500,sX:-1},{b:7500,d:500,sY:1},{b:8000,d:500,sY:-1},{b:8500,d:500,kX:30},{b:9000,d:500,kX:-30},{b:9500,d:500,kY:30},{b:10000,d:500,kY:-30},{b:10500,d:500,c:{x:87.50,t:-87.50}},{b:11000,d:500,c:{x:-87.50,t:87.50}}],
-      [{b:0,d:600,x:410,e:{x:27}}],
-      [{b:-1,d:1,o:-1},{b:0,d:600,o:1,e:{o:5}}],
-      [{b:-1,d:1,c:{x:175.0,t:-175.0}},{b:0,d:800,c:{x:-175.0,t:175.0},e:{c:{x:7,t:7}}}],
-      [{b:-1,d:1,o:-1},{b:0,d:600,x:-570,o:1,e:{x:6}}],
-      [{b:-1,d:1,o:-1,r:-180},{b:0,d:800,o:1,r:180,e:{r:7}}],
-      [{b:0,d:1000,y:80,e:{y:24}},{b:1000,d:1100,x:570,y:170,o:-1,r:30,sX:9,sY:9,e:{x:2,y:6,r:1,sX:5,sY:5}}],
-      [{b:2000,d:600,rY:30}],
-      [{b:0,d:500,x:-105},{b:500,d:500,x:230},{b:1000,d:500,y:-120},{b:1500,d:500,x:-70,y:120},{b:2600,d:500,y:-80},{b:3100,d:900,y:160,e:{y:24}}],
-      [{b:0,d:1000,o:-0.4,rX:2,rY:1},{b:1000,d:1000,rY:1},{b:2000,d:1000,rX:-1},{b:3000,d:1000,rY:-1},{b:4000,d:1000,o:0.4,rX:-1,rY:-1}]
-    ];
 
-    var jssor_1_options = {
-      $AutoPlay: 1,
-      $Idle: 2000,
-      $CaptionSliderOptions: {
-        $Class: $JssorCaptionSlideo$,
-        $Transitions: jssor_1_SlideoTransitions,
-        $Breaks: [
-          [{d:2000,b:1000}]
-        ]
-      },
-      $ArrowNavigatorOptions: {
-        $Class: $JssorArrowNavigator$
-      },
-      $BulletNavigatorOptions: {
-        $Class: $JssorBulletNavigator$
-      }
-    };
 
-    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+$(document).ready(function()
+{
+    /* smooth scrolling for scroll to top */
+	$('#to-top').bind('click', function()
+	{
+		$('body,html').animate({
+			scrollTop: 0},
+			2500);
+	});
 
-    /*responsive code begin*/
-    /*remove responsive code if you don't want the slider scales while window resizing*/
-    function ScaleSlider() {
-        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
-        if (refSize) {
-            refSize = Math.min(refSize, 1170);
-            jssor_1_slider.$ScaleWidth(refSize);
+	//Easing Scroll replace Anchor name in URL and Offset Position
+	$(function(){
+		$('a[href*=#]:not([href=#])').click(function()
+		{
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top -420
+					}, 3500, 'easeOutBounce');
+					return false;
+				}
+			}
+		});
+	});
+});
+$(document).ready(function(){
+    $(".dropdown").hover(
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+            $(this).toggleClass('open');
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+            $(this).toggleClass('open');
         }
-        else {
-            window.setTimeout(ScaleSlider, 30);
-        }
-    }
-    ScaleSlider();
-    $Jssor$.$AddEvent(window, "load", ScaleSlider);
-    $Jssor$.$AddEvent(window, "resize", ScaleSlider);
-    $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
-    /*responsive code end*/
-};
+    );
+});
+
+
+
+$(document).ready(function()
+{
+
+	// Closes the sidebar menu on menu-close button click event
+	$("#menu-close").click(function(e)							//declare the element event ...'(e)' = event (shorthand)
+	{
+																// - will not work otherwise")
+		$("#sidebar-wrapper").toggleClass("active");			//instead on click event toggle active CSS element
+		e.preventDefault(); 									//prevent the default action ("Do not remove as the code
+
+		/*!
+		======================= Notes ===============================
+		* see: .sidebar-wrapper.active in: style.css
+		==================== END Notes ==============================
+		*/
+	});															//Close 'function()'
+
+	// Open the Sidebar-wrapper on Hover
+	$("#menu-toggle").hover(function(e)							//declare the element event ...'(e)' = event (shorthand)
+	{
+		$("#sidebar-wrapper").toggleClass("active",true);		//instead on click event toggle active CSS element
+		e.preventDefault();										//prevent the default action ("Do not remove as the code
+	});
+
+	$("#menu-toggle").bind('click',function(e)					//declare the element event ...'(e)' = event (shorthand)
+	{
+		$("#sidebar-wrapper").toggleClass("active",true);		//instead on click event toggle active CSS element
+		e.preventDefault();										//prevent the default action ("Do not remove as the code
+	});															//Close 'function()'
+
+	$('#sidebar-wrapper').mouseleave(function(e)				//declare the jQuery: mouseleave() event
+																// - see: ('//api.jquery.com/mouseleave/' for details)
+	{
+		/*! .toggleClass( className, state ) */
+		$('#sidebar-wrapper').toggleClass('active',false);		/* toggleClass: Add or remove one or more classes from each element
+																in the set of matched elements, depending on either the class's
+																presence or the value of the state argument */
+		e.stopPropagation();									//Prevents the event from bubbling up the DOM tree
+																// - see: ('//api.jquery.com/event.stopPropagation/' for details)
+
+		e.preventDefault();										// Prevent the default action of the event will not be triggered
+																// - see: ('//api.jquery.com/event.preventDefault/' for details)
+	});
+});
+// Closes the sidebar menu on menu-close button click event
+$("#menu-close").click(function(e)							//declare the element event ...'(e)' = event (shorthand)
+{
+															// - will not work otherwise")
+	$("#sidebar-wrapper").toggleClass("active");			//instead on click event toggle active CSS element
+	e.preventDefault(); 									//prevent the default action ("Do not remove as the code
+
+	/*!
+	======================= Notes ===============================
+	* see: .sidebar-wrapper.active in: style.css
+	==================== END Notes ==============================
+	*/
+});															//Close 'function()'
