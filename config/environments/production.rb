72957a0767bb31d5ae5c2b995335be3bc9f8e7c0
Kingsley <chukwumakingsley1@gmail.com>
2017-06-06 19:21:47 +0200

    fixed broken comment form

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
+
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -87,7 +89,7 @@ end
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
 
 
   config.action_mailer.default_url_options = { host: 'https://rccgkingofglory.herokuapp.com'}
