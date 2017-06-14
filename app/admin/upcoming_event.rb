49ccb16716ed7a41d7b4dd467f863a8a09c08393
Kingsley <chukwumakingsley1@gmail.com>
2017-06-14 23:07:00 +0200

    redesigned and fixed events

--- app/admin/upcoming_event.rb
+++ app/admin/upcoming_event.rb
@@ -1,15 +1,31 @@
 ActiveAdmin.register UpcomingEvent do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  permit_params :title, :body, :starting_date, :going, :end_date, pictures_attributes: [:id, :image, :_destroy ]
 
+  form do |f|
+   f.inputs "Post Details" do
+     f.input :title
+     f.input :body, as: :text
+     f.input :starting_date, :as => :datetime_picker
+     f.input :end_date,:as => :datetime_picker
+     f.input :going, as: :array
+     f.has_many :pictures do |ff|
+       ff.input :image, :as => :file, :hint => ff.template.image_tag(ff.object.image.url(:medium))
+     end
+   end
+   f.actions
+ end
+
+ show do
+   attributes_table do
+     row :title
+     row :body
+     row :starting_date
+     row :end_date
+   end
+    table_for upcoming_event.pictures do
+      column :image do |a|
+        image_tag a.image.url
+      end
+    end
+  end
 end
