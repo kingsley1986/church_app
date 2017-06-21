afc943216b4a498192554eae0dfaeca55ab492d4
Kingsley <chukwumakingsley1@gmail.com>
2017-06-21 19:32:09 +0200

    used try on it

--- app/admin/upcoming_event.rb
+++ app/admin/upcoming_event.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register UpcomingEvent do
-  permit_params :title, :body, :starting_date, :going, :end_date, pictures_attributes: [:id, :image, :_destroy ]
+  permit_params :title, :body, :starting_date, :going, :coming_with, :end_date, pictures_attributes: [:id, :image, :_destroy ]
 
   form do |f|
    f.inputs "Post Details" do
@@ -8,6 +8,7 @@ ActiveAdmin.register UpcomingEvent do
      f.input :starting_date, :as => :datetime_picker
      f.input :end_date,:as => :datetime_picker
      f.input :going, as: :array
+     f.input :coming_with, as: :array
      f.has_many :pictures do |ff|
        ff.input :image, :as => :file, :hint => ff.template.image_tag(ff.object.image.url(:medium))
      end
@@ -19,6 +20,8 @@ ActiveAdmin.register UpcomingEvent do
    attributes_table do
      row :title
      row :body
+     row :going
+     row :coming_with
      row :starting_date
      row :end_date
    end
