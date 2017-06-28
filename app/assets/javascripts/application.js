3d212d7387bf3aba8d862917760720c611c4a84c
Kingsley <chukwumakingsley1@gmail.com>
2017-06-28 17:06:19 +0200

    restored form clear

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,27 +7,40 @@
 
 
 
-$(document).on('click', 'button#send_prayer', function(e) {
+// $(document).on('click', '.tab_cont', function(e) {
+//     $.ajax({
+//         url: $(this).attr('action'),
+//         data: $(this).serialize(),
+//         type: "POST",
+//         dataType: "json",
+//         success: function(response) {
+//             console.log(response)
+//         },
+//         error: function(xhr, textStatus, errorThrown) {
+//
+//           console.log(errorThrown)
+//         },
+//     });
+//     e.preventDefault(); //THIS IS VERY IMPORTANT
+//     $(".tab_cont").trigger('reset');
+// });
+
+
+
+$(document).on('ready page:load', function() {
+  $(".tab_cont").submit(function () {
+    var valuesToSubmit = $(this).serialize();
     $.ajax({
-        url: $(this).attr('action'),
-        data: $(this).serialize(),
-        type: "POST",
-        dataType: "json",
-        success: function(response) {
-            console.log(response)
-        },
-        error: function(xhr, textStatus, errorThrown) {
-
-          console.log(errorThrown)
-        },
+      type: "POST",
+      url: $(this).attr('action'), //sumbits it to the given url of the form
+      data: valuesToSubmit,
+      dataType: "JSON" // you want a difference between normal and ajax-calls, and json is standard
+    }).success(function(json){
+      console.log("success", json);
     });
-    e.preventDefault(); //THIS IS VERY IMPORTANT
     $(".tab_cont").trigger('reset');
-
-
-
-});
-
+  })
+})
 
 $(document).on('click', '.specific_service', function(event) {
   $('.cd-popup').addClass('is-visible');
