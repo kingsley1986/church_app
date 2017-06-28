3f0412c48458730954aae9754b2ac8f7776cbd49
Kingsley <chukwumakingsley1@gmail.com>
2017-06-28 04:32:38 +0200

    fixed the slider and gave it border

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
 //
 //= require jquery
 //= require jquery_ujs
@@ -17,49 +6,30 @@
 //= require_tree .
 
 
-// $(document).on('click', '#goingpeople_upcoming_event', function(event) {
-//   alert("kdjfkdjfkdjfkdjkfdjkfj")
-//   $.ajax({
-//     url: $(this).attr('ajax_path'),
-//     data: { },
-//     async: true,
-//     dataType: 'script'
-//   });
-//   return false;
-// });
 
+$(document).on('click', 'button#send_prayer', function(e) {
+    $.ajax({
+        url: $(this).attr('action'),
+        data: $(this).serialize(),
+        type: "POST",
+        dataType: "json",
+        success: function(response) {
+            console.log(response)
+        },
+        error: function(xhr, textStatus, errorThrown) {
 
-$(document).on('click', '.specific_service', function(event) {
-  $('.cd-popup').addClass('is-visible');
+          console.log(errorThrown)
+        },
+    });
+    e.preventDefault(); //THIS IS VERY IMPORTANT
 
-  //  if (confirm("would you like to come with a friend or family?")) {
-  //    $('.cd-popup').addClass('is-visible');
-   //
-  //    $(".specific_service").hide();
-  //    $("#questionclass").show();
-  //    $("#hiderthisclass").show();
-  //  } else {
-  //    alert("Thank you for chosing to come for this programme")
-   //
-  //  }
 
 });
 
 
-$(document).on('ready page:load', function() {
-  $(".tab_cont").submit(function () {
-    var valuesToSubmit = $(this).serialize();
-    $.ajax({
-      type: "POST",
-      url: $(this).attr('action'), //sumbits it to the given url of the form
-      data: valuesToSubmit,
-      dataType: "JSON" // you want a difference between normal and ajax-calls, and json is standard
-    }).success(function(json){
-    });
-    alert("Your prayer request is being sent to Pastor, do you wish to continue?")
-    $(".tab_cont").trigger('reset');
-  })
-})
+$(document).on('click', '.specific_service', function(event) {
+  $('.cd-popup').addClass('is-visible');
+});
 
 
 $(document).on('click', '.deletecomment', function(event) {
