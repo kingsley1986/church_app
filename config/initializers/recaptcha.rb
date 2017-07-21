cf69e94e2a90c9842b385ea14aafffc162fd7fd2
Kingsley <chukwumakingsley1@gmail.com>
2017-07-21 15:30:20 +0200

    added captca kesy to gitignore

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key   = '6LemMCkUAAAAAD3XE1AnEXf6SMofMYokGS3EMYoX'
-  config.secret_key = '6LemMCkUAAAAAIyc1yBF72uuom4-nm2IWZZLVpWa'
+  config.site_key   = ENV['RECAPTCHA_SITE_KEY']
+  config.secret_key = ENV['RECAPTCHA_PRIVATE_KEY']
 end
