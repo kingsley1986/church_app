a1943e8dfbe508108c28fc0f5cec53be81fb7fcf
Kingsley <chukwumakingsley1@gmail.com>
2017-08-31 12:05:29 +0200

    fixed some styling issues

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -1,34 +1,19 @@
-//
-// $(document).ready(function(){
-//   $('#newcomment')
-//     .bind("ajax:beforeSend", function(evt, xhr, settings){
-//     })
-//     .bind("ajax:success", function(evt, data, status, xhr){
-//       var $form = $(this);
-//
-//       // Reset fields and any validation errors, so form can be used again, but leave hidden_field values intact.
-//       $form.find('textarea,input[type="text"],input[type="file"]').val("");
-//       $form.find('div.validation-error').empty();
-//
-//     });
-// });
+$(function(){
+  $(".contactForm").on("submit", function(event){
+    $.ajax({
+      type: "POST",
+      url: this.action,
+      data: $(this).serialize(),
 
-// $(document).ready(function(){
-//   $('.likecomment')
-//     .bind("ajax:beforeSend", function(evt, xhr, settings){
-//     })
-//     .bind("ajax:success", function(evt, data, status, xhr){
-//       var valuesToSubmit = $(this).serialize();
-//       $.ajax({
-//         type: "POST",
-//         url: $(this).attr('action'), //sumbits it to the given url of the form
-//         data: valuesToSubmit,
-//         dataType: "JSON" // you want a difference between normal and ajax-calls, and json is standard
-//       }).success(function(json){
-//         console.log("success", json);
-//       });
-//       $(".unlikecomment").trigger('reset');
-//       return false; // prevents normal behaviour
-//
-//     });
-// });
+    success: function(response) {
+        if (response == "success")
+        alert("")
+        else
+        $(".message-class").html("<span class='text-error marginl10' ><b>Not sent, please fill in the correct details..</b></span>")
+    }
+    });
+    event.preventDefault();
+    $(".contactForm").trigger('reset');
+
+  });
+})
