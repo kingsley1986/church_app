680905e90dc785b6d5beb621219e3d452eec11c0
kingsley <chukwumakingsley1@gmail.com>
2019-08-22 20:51:17 +0200

    removed custom font css file for the event links

--- app/helpers/upcoming_events_helper.rb
+++ app/helpers/upcoming_events_helper.rb
@@ -1,2 +1,9 @@
 module UpcomingEventsHelper
+  # 
+  # def upcomings
+  #   @ue = UpcomingEvent.all
+  #   upcomings = @ue.select { |a| a.starting_date.strftime('%Y-%m-%d %H:%M:%S') > DateTime.now.strftime('%Y-%m-%d %H:%M:%S')}
+  #     return upcomings
+  # end
+
 end
