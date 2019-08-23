c02b423a47154e8e34e47982435f6d983ad61252
kingsley <chukwumakingsley1@gmail.com>
2019-08-23 15:29:50 +0200

    changed the upcomings events to show on 4 rows and read more

--- Gemfile
+++ Gemfile
@@ -19,7 +19,7 @@ gem "mini_magick"
 gem "fog"
 gem "figaro"
 gem 'rails_12factor', group: :production
-gem 'will_paginate'
+gem 'will_paginate', '~> 3.1', '>= 3.1.8'
 gem 'jquery-turbolinks'
 gem "font-awesome-rails"
 gem 'mail_form', '~> 1.7', '>= 1.7.1'
--- app/helpers/upcoming_events_helper.rb
+++ app/helpers/upcoming_events_helper.rb
@@ -1,9 +1,9 @@
 module UpcomingEventsHelper
-  # 
-  # def upcomings
-  #   @ue = UpcomingEvent.all
-  #   upcomings = @ue.select { |a| a.starting_date.strftime('%Y-%m-%d %H:%M:%S') > DateTime.now.strftime('%Y-%m-%d %H:%M:%S')}
-  #     return upcomings
-  # end
+
+  def upcomings
+    @ue = UpcomingEvent.paginate(page: params[:page], per_page:  4).all.order('created_at DESC')
+    upcomings = @ue.select { |a| a.starting_date.strftime('%Y-%m-%d %H:%M:%S') > DateTime.now.strftime('%Y-%m-%d %H:%M:%S')}
+      return upcomings
+  end
 
 end
